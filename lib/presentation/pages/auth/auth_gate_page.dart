afb0c6faf3fbb572c50055ff36c8320268d61c51
AlvianAhmad <alalvian200@gmail.com>
2025-12-25 12:50:17 +0700

    fitur backend admin (done)

--- lib/presentation/pages/auth/auth_gate_page.dart
+++ lib/presentation/pages/auth/auth_gate_page.dart
@@ -3,72 +3,45 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import '../../../routes/app_routes.dart';
 
-class AuthGatePage
-    extends
-        StatelessWidget {
-  const AuthGatePage({
-    super.key,
-  });
+class AuthGatePage extends StatelessWidget {
+  const AuthGatePage({super.key});
 
   @override
-  Widget build(
-    BuildContext context,
-  ) {
-    return StreamBuilder<
-      User?
-    >(
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
-      builder:
-          (
-            context,
-            snapshot,
-          ) {
-            // loading
-            if (snapshot.connectionState ==
-                ConnectionState.waiting) {
-              return const Scaffold(
-                body: Center(
-                  child: CircularProgressIndicator(),
-                ),
-              );
-            }
+      builder: (context, snapshot) {
+        // loading
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
 
-            // ===== LOGOUT / BELUM LOGIN =====
-            if (!snapshot.hasData) {
-              WidgetsBinding.instance.addPostFrameCallback(
-                (
-                  _,
-                ) {
-                  Navigator.pushNamedAndRemoveUntil(
-                    context,
-                    AppRoutes.login,
-                    (
-                      route,
-                    ) => false,
-                  );
-                },
-              );
-              return const SizedBox();
-            }
-
-            // ===== LOGIN =====
-            WidgetsBinding.instance.addPostFrameCallback(
-              (
-                _,
-              ) {
-                // sementara langsung admin
-                Navigator.pushNamedAndRemoveUntil(
-                  context,
-                  AppRoutes.admin,
-                  (
-                    route,
-                  ) => false,
-                );
-              },
+        // ===== LOGOUT / BELUM LOGIN =====
+        if (!snapshot.hasData) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            Navigator.pushNamedAndRemoveUntil(
+              context,
+              AppRoutes.login,
+              (route) => false,
             );
+          });
+          return const SizedBox();
+        }
 
-            return const SizedBox();
-          },
+        // ===== LOGIN =====
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          // sementara langsung admin
+          Navigator.pushNamedAndRemoveUntil(
+            context,
+            AppRoutes.admin,
+            (route) => false,
+          );
+        });
+
+        return const SizedBox();
+      },
     );
   }
 }
