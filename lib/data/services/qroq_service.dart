05c6a2c8c6daedb7f6caa33fee92bc4332baeee9
AlvianAhmad <alalvian200@gmail.com>
2026-01-10 20:50:30 +0700

    rubah apikey

--- lib/data/services/qroq_service.dart
+++ lib/data/services/qroq_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class GroqService {
   static const String _apiKey =
-      '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';
+      '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';
   static const String _endpoint =
       'https://api.groq.com/openai/v1/chat/completions';
 
